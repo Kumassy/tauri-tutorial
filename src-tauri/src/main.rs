a3db4eb335ef42a615fea352aedbf69522ebc5b2
Kumassy <kumassyii@gmail.com>
2022-05-07 23:45:33 +0900

    chap: command:

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,61 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
+use serde::{ Serialize, Deserialize };
+
+#[tauri::command]
+fn simple_command() {
+    println!("I was invoked from JS!");
+}
+
+#[tauri::command]
+fn command_with_message(message: String) -> String {
+    format!("hello {}", message)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyMessage {
+    field_str: String,
+    field_u32: u32,
+}
+
+#[tauri::command]
+fn command_with_object(message: MyMessage) -> MyMessage {
+    let MyMessage {
+        field_str,
+        field_u32,
+    } = message;
+
+    MyMessage {
+        field_str: format!("hello {}", field_str),
+        field_u32: field_u32 + 1,
+    }
+}
+
+#[tauri::command]
+fn command_with_error(arg: u32) -> Result<String, String> {
+    if arg % 2 == 0 {
+        Ok(format!("even value {}", arg))
+    } else {
+        Err(format!("odd value {}", arg))
+    }
+}
+
+#[tauri::command]
+async fn async_command(arg: u32) -> String {
+    "hello".into()
+}
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            simple_command,
+            command_with_message,
+            command_with_object,
+            command_with_error,
+            async_command,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { invoke } from '@tauri-apps/api/tauri'
 
 function App() {
+  function executeCommands() {
+    invoke('simple_command')
+    invoke('command_with_message', { message: 'some message' }).then(message => {
+      console.log('command_with_messge', message)
+    })
+    invoke('command_with_object', { message: { field_str: 'some message', field_u32: 12 }}).then(message => {
+      console.log('command_with_object', message)
+    })
+
+    for (let arg of [1, 2]) {
+      invoke('command_with_error', { arg }).then(message => {
+        console.log('command_with_error', message)
+      }).catch(message => {
+        console.error('command_with_error', message)
+      })
+    }
+
+    invoke('async_command', { arg: 14 }).then(message => {
+      console.log('async_command', message)
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +35,7 @@ function App() {
           <br></br>
           Hello Tauri
         </p>
+        <button onClick={executeCommands}>Click to execute command</button>
         <a
           className="App-link"
           href="https://reactjs.org"
