4ea608ee85827cf9ab5f470da64e0f8479ff5f82
Kumassy <kumassyii@gmail.com>
2023-03-28 22:25:14 +0900

    recreate app by create-tauri-app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,79 +1,15 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
-use serde::{Deserialize, Serialize};
-use tauri::Manager;
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn simple_command() {
-    println!("I was invoked from JS!");
-}
-
-#[tauri::command]
-fn command_with_message(message: String) -> String {
-    format!("hello {}", message)
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct MyMessage {
-    field_str: String,
-    field_u32: u32,
-}
-
-#[tauri::command]
-fn command_with_object(message: MyMessage) -> MyMessage {
-    let MyMessage {
-        field_str,
-        field_u32,
-    } = message;
-
-    MyMessage {
-        field_str: format!("hello {}", field_str),
-        field_u32: field_u32 + 1,
-    }
-}
-
-#[tauri::command]
-fn command_with_error(arg: u32) -> Result<String, String> {
-    if arg % 2 == 0 {
-        Ok(format!("even value {}", arg))
-    } else {
-        Err(format!("odd value {}", arg))
-    }
-}
-
-#[tauri::command]
-async fn async_command(arg: u32) -> String {
-    "hello".into()
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            let id = app.listen_global("front-to-back", |event| {
-                println!(
-                    "got front-to-back with payload {:?}",
-                    event.payload().unwrap()
-                )
-            });
-
-            let app_handle = app.app_handle();
-            std::thread::spawn(move || loop {
-                app_handle
-                    .emit_all("back-to-front", "ping frontend".to_string())
-                    .unwrap();
-                std::thread::sleep(std::time::Duration::from_secs(1))
-            });
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![
-            simple_command,
-            command_with_message,
-            command_with_object,
-            command_with_error,
-            async_command,
-        ])
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,51 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { invoke } from '@tauri-apps/api/tauri'
-import { emit, listen } from '@tauri-apps/api/event'
-import { open } from '@tauri-apps/api/dialog'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
 
 function App() {
-  function executeCommands() {
-    invoke('simple_command')
-    invoke('command_with_message', { message: 'some message' }).then(message => {
-      console.log('command_with_messge', message)
-    })
-    invoke('command_with_object', { message: { field_str: 'some message', field_u32: 12 }}).then(message => {
-      console.log('command_with_object', message)
-    })
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
 
-    for (let arg of [1, 2]) {
-      invoke('command_with_error', { arg }).then(message => {
-        console.log('command_with_error', message)
-      }).catch(message => {
-        console.error('command_with_error', message)
-      })
-    }
-
-    invoke('async_command', { arg: 14 }).then(message => {
-      console.log('async_command', message)
-    })
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
   }
 
-  function openDialog () {
-    open().then(files => console.log(files))
-  }
-
-  function emitMessage() {
-    emit('front-to-back', "hello from front")
-  }
-
-  useEffect(() => {
-    let unlisten: any;
-    async function f() {
-      unlisten = await listen('back-to-front', event => {
-        console.log(`back-to-front ${event.payload} ${new Date()}`)
-      });
-    }
-    f();
-
-    return () => {
-      if (unlisten) {
-        unlisten();
-      }
-    }
-  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="container">
+      <h1>Welcome to Tauri!</h1>
 
-          <br></br>
-          Hello Tauri
-        </p>
-        <button onClick={executeCommands}>Click to execute command</button>
-        <button onClick={openDialog}>Click to open dialog</button>
-        <button onClick={emitMessage}>Click to emit message</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
-      </header>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <div className="row">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            greet();
+          }}
+        >
+          <input
+            id="greet-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter a name..."
+          />
+          <button type="submit">Greet</button>
+        </form>
+      </div>
+      <p>{greetMsg}</p>
     </div>
   );
 }
